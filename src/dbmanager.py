10d7cd66bc31bf384d9ae945dcf9a5f792b47852
Giz <Jeppe1000@live.dk>
2020-12-06 15:49:45 +0100

    Made some error handling, and adjustments to json responses

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -10,22 +10,38 @@ def get_all_comments(session):
     comments = session.query(Comment).all()
     return comments
 
+def remove_all_comments(session):
+    comments = get_all_comments(session)
+    for comment in comments:
+        delete_comment(session, comment.authorId, comment.id)
+
+'''
+For testing
+'''
+def size_of_db(session):
+    size = 0
+    comments = session.query(Comment).all()
+    for comment in comments:
+        size = size + 1
+    return size
+
 '''
 @Input - A session, and comment Id(cId)
 @Output - Json object containing the comment's id, post id, 
 ISO8601 timestamp(when it was created), and the content.
 '''
 def request_comment(session, cId):
-    comment = session.query(Comment).get(cId)
-    return json.dumps({'comment-id': comment.id, 'post-id': comment.postId, 'created-at': comment.postedAt, 'content': comment.content}, indent=2, default=str)
-  
+    try:
+        comment = session.query(Comment).get(cId)
+        return json.dumps({'comment-id': comment.id, 'post-id': comment.postId, 'created-at': comment.postedAt, 'content': comment.content, 'http-response': 200}, indent=2, default=str)
+    except:
+        return json.dumps({'comment-id': cId, 'post-id': 999999, 'created-at': 999999, 'content': "Could not find comment", 'http-response': 403}, indent=2, default=str)
 '''
 @Input - A session, authorId, postId, content
 @Output - The created comment's id, ISO8601 timestamp for when it was created, 
 and http response code.
 '''
 def create_comment(session, authorId, postId, content):
-    #session = Session()
     comment = Comment()
     comment.authorId = authorId
     comment.postId = postId
@@ -44,14 +60,17 @@ and the new content.
 ISO8601 timestamp representing when it was modified.
 '''
 def update_comment(session, cId, aId, newContent):
-    comment = session.query(Comment).get(cId)
-    if(aId == comment.authorId):
-        comment.content = newContent
-        comment.postedAt = datetime.now().isoformat()
-        session.commit()
-        return json.dumps({'confirm-delete': 200, 'comment-id': comment.id, 'update-timestamp': comment.postedAt}, indent=2, default=str)
-    else:
-        return json.dumps({'confirm-delete': 403, 'comment-id': comment.id, 'update-timestamp': comment.postedAt}, indent=2, default=str)
+    try:
+        comment = session.query(Comment).get(cId) # Might not exist
+        if(aId == comment.authorId):
+            comment.content = newContent
+            comment.postedAt = datetime.now().isoformat()
+            session.commit()
+            return json.dumps({'http-response': 200, 'comment-id': comment.id, 'update-timestamp': comment.postedAt}, indent=2, default=str)
+        else:
+            return json.dumps({'http-response': 403, 'comment-id': comment.id, 'update-timestamp': comment.postedAt}, indent=2, default=str)
+    except:
+        return json.dumps({'http-response': 403, 'comment-id': cId, 'update-timestamp': 1000001}, indent=2, default=str)
         
 '''
 @Input - A session, id of the user/author wanting to delete comment,
@@ -60,13 +79,16 @@ and the id of the comment
 '''
 def delete_comment(session, aId, cId):
     #apparently it has to be done in two steps. Can't just call .delete()
-    comment = session.query(Comment).get(cId)
-    if(aId == comment.authorId):
-        session.delete(comment)
-        session.commit()
-        return json.dumps({'confirm-delete': 200}, indent=2, default=str)
-    else:
-        return json.dumps({'confirm-delete': 403}, indent=2, default=str)
+    try:
+        comment = session.query(Comment).get(cId) # Might not exist
+        if(aId == comment.authorId):
+            session.delete(comment)
+            session.commit()
+            return json.dumps({'http-response': 200}, indent=2, default=str)
+        else:
+            return json.dumps({'http-response': 403}, indent=2, default=str)
+    except:
+        return json.dumps({'http-response': 403}, indent=2, default=str)
 
     
 '''
@@ -79,4 +101,3 @@ def request_comments_for_post(session, pId):
     for comment in comments:
         commentIdArray.append(comment.id)
     return json.dumps({'list-of-comment-ids': commentIdArray}, indent=2, default=str)
-    
