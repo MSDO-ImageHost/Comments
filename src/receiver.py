49e1c6780e1c2f7b4553bb319f7c6ea943da9795
Giz <Jeppe1000@live.dk>
2020-12-06 22:39:37 +0100

    Modified the rabbitMQ messages with properties. Also checks if JWT is valid before doing operations.

--- src/receiver.py
+++ src/receiver.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from dbmanager import get_all_comments, request_comment, update_comment, delete_comment, request_comments_for_post, create_comment
 from config import DATABASE_CONNECTION_URI
 from models import Base, Comment
+from jose import jwt
 import json
 import pika
 import sys
+import time
 
 # For the db part
 engine = create_engine(DATABASE_CONNECTION_URI, echo = True) #Connect to a specific database, remove echo later
@@ -21,37 +23,67 @@ events = ['CreateComment', 'UpdateComment', 'DeleteComment', 'RequestComment', '
 @Input: Event string, json data(dict)
 @Output: None, puts object onto rapid
 '''
-def send(event, data):
-    channel.basic_publish(exchange='rapid', routing_key=event, body=data)
+def send(event, data, properties):
+    channel.basic_publish(exchange='rapid', routing_key=event, body=data, properties=properties)
+
+#It decodes the token and makes sure that the issuer is what we expect and that it has not expired
+def verify(token):
+    try:
+        decoded = jwt.decode(token, "secret", algorithms=['HS256'])
+        millis = int(round(time.time() * 1000))
+        if (decoded["iss"] == "ImageHost.sdu.dk" and decoded["exp"] < millis):
+            return decoded
+        else:
+            return None
+    except:
+        return None
 
 '''
 @Input: event string, json data(dict)
 @Output: Nothing, calls a function to put object onto rapid
 '''
-def receive(event, data):
+def receive(event, data, properties):
     data = json.loads(data)
-    if event == "CreateComment":
-        jsonObject = create_comment(session, data['user-id'], data['post-id'], data['content'])
-        send("ConfirmCommentCreation", jsonObject)
-    elif event == "UpdateComment":
-        jsonObject = update_comment(session, data['comment-id'], data['user-id'], data['content'])
-        send("ConfirmCommentUpdate", jsonObject)
-    elif event == "DeleteComment":
-        jsonObject = delete_comment(session, data['user-id'], data['comment-id'])
-        send("ConfirmCommentDelete", jsonObject)
-    elif event == "RequestComment":
-        jsonObject = request_comment(session, data['comment-id'])
-        send("ReturnComment", jsonObject)
-    elif event == "RequestCommentsForPost":
-        jsonObject = request_comments_for_post(session, data['post-id'])
-        send("ReturnCommentsForPost", jsonObject)
+
+    if(verify(properties.headers['jwt'])):
+        if event == "CreateComment":
+            jsonObject = create_comment(session, data['user-id'], data['post-id'], data['content'])
+            # Get the actual http response from the action and put it into properties
+            httpResponse = json.loads(jsonObject)['http-response']
+            properties.headers['http-response'] = httpResponse
+            send("ConfirmCommentCreation", jsonObject, properties)
+        elif event == "UpdateComment":
+            jsonObject = update_comment(session, data['comment-id'], data['user-id'], data['content'])
+            httpResponse = json.loads(jsonObject)['http-response']
+            properties.headers['http-response'] = httpResponse
+            send("ConfirmCommentUpdate", jsonObject, properties)
+        elif event == "DeleteComment":
+            jsonObject = delete_comment(session, data['user-id'], data['comment-id'])
+            httpResponse = json.loads(jsonObject)['http-response']
+            properties.headers['http-response'] = httpResponse
+            send("ConfirmCommentDelete", jsonObject, properties)
+        elif event == "RequestComment":
+            jsonObject = request_comment(session, data['comment-id'])
+            httpResponse = json.loads(jsonObject)['http-response']
+            properties.headers['http-response'] = httpResponse
+            send("ReturnComment", jsonObject, properties)
+        elif event == "RequestCommentsForPost":
+            jsonObject = request_comments_for_post(session, data['post-id'])
+            send("ReturnCommentsForPost", jsonObject, properties)
+        else:
+            pass
+    else:
+        # What should happen if an invalid jwt was given?
+        pass
 
 def callback(channel, method, properties, body):
     event = method.routing_key
+    print("###################################")
     print(method)
     print(properties)
     print(body)
-    print(receive(event, body))
+    print("###################################")
+    receive(event, body, properties)
 
 if __name__ == '__main__':
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
--- src/sender.py
+++ src/sender.py
@@ -6,8 +6,16 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 channel.exchange_declare(exchange='rapid', exchange_type='direct')
 
-jsonob = json.dumps({'user-id': 3, 'post-id': 5, 'content': "AM I WORKING PLEASE??"}, indent=2, default=str)
+jsonob = json.dumps({'user-id': 3, 'post-id': 5, 'content': "is me, your boi"}, indent=2, default=str)
 event = "CreateComment"
+hdrs = {'jwt': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWiwicm9sZSI6MSwiaXNzIjoiSW1hZ2VIb3N0LnNkdS5kayIsImV4cCI6MTYwODUyMDMyOSwiaWF0IjoxNjA3MjA2MzI5fQ.EFv8rHJYAp0DE8h2GFzZzceCiOZS4ZfCh6aBkIHNsEs', 'http-response': '200'}
 
-channel.basic_publish(exchange='rapid', routing_key=event, body=jsonob)
+properties = pika.BasicProperties(correlation_id='1337',
+    content_type='application/json',  
+    headers=hdrs)
+
+channel.basic_publish(exchange='rapid',
+                        routing_key=event, 
+                        body=jsonob, 
+                        properties=properties)
 connection.close()
