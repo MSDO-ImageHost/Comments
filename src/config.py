b67656e350d7913d3c41a8d8c98afe1f056077a6
Giz <Jeppe1000@live.dk>
2020-12-16 19:18:50 +0100

    Changed dialect and changed from postgre to mySQL database.

--- src/config.py
+++ src/config.py
@@ -9,13 +9,19 @@ DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT
 '''
 import os
 
+USER = os.environ['MYSQL_USER']
+PASSWORD = os.environ['MYSQL_PASSWORD']
+HOST = "mySQL"
+DATABASE = os.environ['MYSQL_DATABASE']
+DATABASE_CONNECTION_URI = f'mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:3306/{DATABASE}'
+'''
 USER = os.environ['POSTGRES_USER']
 PASSWORD = os.environ['POSTGRES_PASSWORD']
 HOST = os.environ['POSTGRES_HOST']
 PORT = os.environ['POSTGRES_PORT']
 DATABASE = os.environ['POSTGRES_DB']
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
-
-AMQP_USER = PORT = os.environ['RABBITMQ_USERNAME']
-AMQP_PASSWORD = os.environ['RABBITMQ_PASSWORD']
+'''
+AMQP_USER = PORT = os.environ['RABBITMQ_DEFAULT_USER']
+AMQP_PASSWORD = os.environ['RABBITMQ_DEFAULT_PASS']
 AMQP_URI = f"amqp://{AMQP_USER}:{AMQP_PASSWORD}@rabbitmq:5432/"
