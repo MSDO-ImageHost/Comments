a5e90c23fe622c78bfbaf4c1e7f1ac9cf4303a92
Giz <Jeppe1000@live.dk>
2020-12-16 20:00:13 +0100

    Changed compose + clean up

--- src/config.py
+++ src/config.py
@@ -1,12 +1,3 @@
-'''
-USER = "root"
-PASSWORD = "toor"
-HOST = "database"
-PORT = 5432
-DATABASE = "commentdb"
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
-
-'''
 import os
 
 USER = os.environ['MYSQL_USER']
@@ -14,14 +5,7 @@ PASSWORD = os.environ['MYSQL_PASSWORD']
 HOST = "mySQL"
 DATABASE = os.environ['MYSQL_DATABASE']
 DATABASE_CONNECTION_URI = f'mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:3306/{DATABASE}'
-'''
-USER = os.environ['POSTGRES_USER']
-PASSWORD = os.environ['POSTGRES_PASSWORD']
-HOST = os.environ['POSTGRES_HOST']
-PORT = os.environ['POSTGRES_PORT']
-DATABASE = os.environ['POSTGRES_DB']
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
-'''
+
 AMQP_USER = PORT = os.environ['RABBITMQ_DEFAULT_USER']
 AMQP_PASSWORD = os.environ['RABBITMQ_DEFAULT_PASS']
 AMQP_URI = f"amqp://{AMQP_USER}:{AMQP_PASSWORD}@rabbitmq:5432/"
